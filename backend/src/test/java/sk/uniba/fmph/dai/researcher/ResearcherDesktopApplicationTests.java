ddd0db9cf082cd985e7d499cc2979bc6db181e8b
honcharenko2 <honcharenko2@uniba.sk>
2026-03-13 12:35:34 +0100

    Initial project commit

---
+++ backend/src/test/java/sk/uniba/fmph/dai/researcher/ResearcherDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.uniba.fmph.dai.researcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearcherDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}o
